f5f4f1002110d64fe2ea0745d02eeb08aef3d434
Leon Ho <leon.ho@koeitecmo.com.sg>
2014-11-17 15:31:23 +0800

    converted agenda to use a function instead of spawning new process

--- agenda.js
+++ agenda.js
@@ -2,20 +2,36 @@ var Agenda = require('agenda');
 var util = require('util');
 var spawn = require('child_process').spawn;
 
+var updateMetrics = require('./jobs/update-metrics');
+
 var agenda = new Agenda();
 
 agenda.database('localhost:27017/annie', 'agenda');
 
+// spawn process version
+
+// agenda.define('update metrics', function(job, done){
+//   var job = spawn('node', ['jobs/update-metrics.js']);
+//   job.stderr.on('data', function (data) {
+//     util.puts('stderr: ' + data);
+//   });
+//   job.stdout.on('data', function (data) {
+//     util.puts(Date() + '> stdout: ' + data);
+//   });
+//   job.on('close', function(code){
+//     util.puts('update metrics job exited with code ' + code);
+//     done();
+//   });
+// });
+
+// agenda.every('*/1 * * * *', 'update metrics');
+
+// function version
+
 agenda.define('update metrics', function(job, done){
-  var job = spawn('node', ['jobs/update-metrics.js']);
-  job.stderr.on('data', function (data) {
-    util.puts('stderr: ' + data);
-  });
-  job.stdout.on('data', function (data) {
-    util.puts(Date() + '> stdout: ' + data);
-  });
-  job.on('close', function(code){
-    util.puts('update metrics job exited with code ' + code);
+  updateMetrics().then(function(apps){
+    done();
+  }, function(err){
     done();
   });
 });
--- jobs/update-metrics.js
+++ jobs/update-metrics.js
@@ -8,40 +8,83 @@ var ApplicationModel = require('../models').ApplicationModel;
 
 var compute = require('../core/metric').compute;
 
-ApplicationModel.find({}).exec(function(err, apps){
-  var counter = 0;
-  apps.forEach(function(app){
-    app.metrics.forEach(function(metric){
-      counter += 1;
-      compute(metric.expression)
-        .then(function(value){
-          if (metric.settings.format.length !== 0){
-            value = sprintf(metric.settings.format, value);
-          }
-          metric.set('value', value, String);
-          // save changes to the app after metric is updated
-          counter -= 1;
-          if (counter === 0) {
-            apps.map(function(app){
-              counter += 1;
-              app.save(function(err, app, n){
-                if (err) {
-                  console.log(err);
-                  process.exit(1);
-                } else {
-                  counter -= 1;
-                  if (counter === 0){
-                    process.exit(0);
-                  }
-                }
-              });
-            });
-          }
-        })
-        .catch(function(err){
-          console.log(err);
-          process.exit(1);
+// process version
+
+// ApplicationModel.find({}).exec(function(err, apps){
+//   var counter = 0;
+//   apps.forEach(function(app){
+//     app.metrics.forEach(function(metric){
+//       counter += 1;
+//       compute(metric.expression)
+//         .then(function(value){
+//           if (metric.settings.format.length !== 0){
+//             value = sprintf(metric.settings.format, value);
+//           }
+//           metric.set('value', value, String);
+//           // save changes to the app after metric is updated
+//           counter -= 1;
+//           if (counter === 0) {
+//             apps.map(function(app){
+//               counter += 1;
+//               app.save(function(err, app, n){
+//                 if (err) {
+//                   console.log(err);
+//                   process.exit(1);
+//                 } else {
+//                   counter -= 1;
+//                   if (counter === 0){
+//                     process.exit(0);
+//                   }
+//                 }
+//               });
+//             });
+//           }
+//         })
+//         .catch(function(err){
+//           console.log(err);
+//           process.exit(1);
+//         });
+//     });
+//   });
+// });
+
+// function version
+
+var updateMetrics = function(){
+
+  return new RSVP.Promise(function(resolve, reject){
+    ApplicationModel.find({}).exec(function(err, apps){
+      var promisedAppUpdates = apps.map(function(app){
+        var promisedMetricUpdates = app.metrics.map(function(metric){
+          // compute returns a promise
+          return compute(metric.expression);
         });
+        return new RSVP.Promise(function(resolve, reject){
+          RSVP.all(promisedMetricUpdates).then(function(values){
+            app.metrics.forEach(function(metric, i){
+              var value = values[i];
+              if (metric.settings.format.length !== 0){
+                value = sprintf(metric.settings.format, value);
+              }
+              // metric.set('value', value, String);
+              metric.set('value', value);
+            });
+            app.save(function(err, app, n){
+              resolve(app);
+              reject(err);
+            })
+          });
+        });
+      });
+
+      RSVP.all(promisedAppUpdates).then(function(apps){
+        resolve(apps);
+      }).catch(function(err){
+        console.error(err);
+        reject(err);
+      });
     });
   });
-});
+};
+
+module.exports = updateMetrics;
