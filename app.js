0e464f9bf48c7fcccb1bb1e7c9ca72fc0d0d10e6
Leon Ho <leon.ho@koeitecmo.com.sg>
2014-11-19 11:58:52 +0800

    add table and chart views

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ hbs.registerHelper('format-markdown', require('./views/helpers/format-markdown')
 hbs.registerPartials(__dirname + '/views/partials');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
---
+++ views/helpers/render-metric-partial.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var path = require('path');
+var hbs = require('hbs');
+
+module.exports = function(){
+  var source;
+  switch(this.settings.metricType){
+    case 'value':
+      break;
+    case 'table':
+      break;
+    case 'line_chart':
+    case 'pie_chart':
+      if (typeof this.value === 'object') {
+        this.value = JSON.stringify(this.value);
+      }
+      break;
+  }
+  source = fs.readFileSync(path.join(__dirname, '../partials/metric_' + this.settings.metricType + '.hbs'), { encoding: 'utf8' });
+  source = '<div class="well metric-item" data-metric-id="{{_id}}">' + source + '</div>';
+  var template = hbs.handlebars.compile(source);
+  return template(this);
+}
